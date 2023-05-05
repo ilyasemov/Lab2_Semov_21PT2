f8216ac59b7b2561d42c0b9140a22500318f9eca
ilyasemov <115580012+ilyasemov@users.noreply.github.com>
2023-05-05 03:07:28 +0300

    Update main.cpp

--- 2.1/main.cpp
+++ 2.1/main.cpp
@@ -1,34 +1,42 @@
 #include <iostream>
 #include <cctype>
-#include "modAlphaCipher.h"
 #include <locale>
-#include <codecvt>
-#include <typeinfo>
+#include "Cipher.h"
 using namespace std;
 void check(const wstring& Text, const wstring& key, const bool destructCipherText=false)
 {
     try {
         wstring cipherText;
         wstring decryptedText;
-        modAlphaCipher cipher(key);
+        Cipher cipher(key);
         cipherText = cipher.encrypt(Text);
         if (destructCipherText)
-            cipherText.front() = towlower(cipherText.front());
+            cipherText.front() = tolower(cipherText.front());
         decryptedText = cipher.decrypt(cipherText);
-        wcout<<L"key="<<key<<endl;
+        wcout<<"key="<<key<<endl;
         wcout<<Text<<endl;
         wcout<<cipherText<<endl;
         wcout<<decryptedText<<endl;
     } catch (const cipher_error & e) {
-        wcerr<<"Error: "<<e.what()<<endl;
+        cerr<<"Error: "<<e.what()<<endl;
     }
-    
 }
-
-int main()
+int main(int argc, char **argv)
 {
-    check(L"student",L"ilya");
-    check(L"ilya",L"");
-    check(L"ilya",L"student123");
-    check(L"i l y a",L"STUDENT");
-    check(L"123",L"STUDENT");
+    locale loc("ru_RU.UTF-8");
+    locale::global(loc);
+    wcout<<L'-'<<endl;
+    check(L"ВСЕМПРИВЕТ",L"ПРИВЕТ");
+    wcout<<L'-'<<endl;
+    check(L"ВСЕМПРИВЕТ",L"");
+    wcout<<L'-'<<endl;
+    check(L"ВСЕМПРИВЕТ",L"П1Р2И3В4Е5Т6");
+    wcout<<L'-'<<endl;
+    check(L"ВСЕМ ПРИВЕТ ЭТО ИЛЬЯ",L"ПРИВЕТ");
+    wcout<<L'-'<<endl;
+    check(L"ВСЕМПРИВЕТЭТО",L"ПРИВЕТ",true);
+    wcout<<L'-'<<endl;
+    check(L"32131231234353223543",L"ПРИВЕТ");
+    wcout<<L'-'<<endl;
+    return 0;
+}
