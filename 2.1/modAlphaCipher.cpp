aa27952354961003d69ae8da898e582e0fd23081
ilyasemov <115580012+ilyasemov@users.noreply.github.com>
2023-05-23 00:37:55 +0300

    Update modAlphaCipher.cpp

--- 2.1/modAlphaCipher.cpp
+++ 2.1/modAlphaCipher.cpp
@@ -1,82 +1,57 @@
 #include "modAlphaCipher.h"
-modAlphaCipher::modAlphaCipher(
-    const std::wstring& wskey)
+
+std::string modAlphaCipher::encrypt(const std::string& open_text)
 {
-    for (unsigned i=0; i<numAlpha.size(); i++) {
-        alphaNum[numAlpha[i]]=i;
-    }
-    key = convert(getValidKey(wskey));
-}
-std::wstring modAlphaCipher::encrypt(const std::wstring& open_text)
-{
-    std::vector<int> work = convert(getValidOpenText(open_text));
-    for(unsigned i=0; i < work.size(); i++) {
-        work[i] = (work[i] + key[i % key.size()]) % alphaNum.size();
-    }
-    return convert(work);
-}
-std::wstring modAlphaCipher::decrypt(const std::wstring& cipher_text)
-{
-    std::vector<int> work = convert(getValidCipherText(cipher_text));
-    for(unsigned i=0; i < work.size(); i++) {
-        work[i] = (work[i] + alphaNum.size() - key[i % key.size()]) % alphaNum.size();
-    }
-    return convert(work);
-}
-inline std::vector<int> modAlphaCipher::convert(const std::wstring& ws)
-{
-    std::vector<int> result;
-    for(auto c:ws) {
-        result.push_back(alphaNum[c]);
-    }
-    return result;
-}
-inline std::wstring modAlphaCipher::convert(const std::vector<int>& v)
-{
-    std::wstring result;
-    for(auto i:v) {
-        result.push_back(numAlpha[i]);
-    }
-    return result;
-}
-inline std::wstring modAlphaCipher::getValidKey(const std::wstring & ws)
-{
-    if (ws.empty())
-        throw cipher_error("Пустой ключ");
-    std::wstring tmp(ws);
-    std::string s = codec.to_bytes(ws);
-    for (auto & c:tmp) {
-        if (!iswalpha(c)) {
-            throw cipher_error(std::string("Неправильный тип ключа ") + s);
+    string s = getValidText(open_text);
+    string n_s = s;
+    int dl, nstrok, index, k;
+    dl = s.length();
+    nstrok = (dl - 1) / key + 1;
+    k = 0;
+    for (int i = key; i > 0; i--) {
+        for (int j = 0; j < nstrok; j++) {
+            index = i+key*j;
+            if (index-1 < dl) {
+                n_s[k] = s[index-1];
+                k++;
+            }
         }
-        if (iswlower(c))
-            c = towupper(c);
     }
-    return tmp;
+    return n_s;
 }
-inline std::wstring modAlphaCipher::getValidOpenText(const std::wstring & ws)
+
+std::string modAlphaCipher::decrypt(const std::string& cipher_text)
 {
-    std::wstring tmp;
-    for (auto c:ws) {
-        if (iswalpha(c)) {
-            if (iswlower(c))
-                tmp.push_back(towupper(c));
+    string s = getValidText(cipher_text);
+    string n_s = s;
+    int dl, nstrok, index, k;
+    dl = s.length();
+    nstrok = (dl - 1) / key + 1;
+    k = 0;
+    for (int i = key; i > 0; i--) {
+        for (int j = 0; j < nstrok; j++) {
+            index = i+key*j;
+            if (index-1 < dl) {
+                n_s[index-1] = s[k];
+                k++;
+            }
+        }
+    }
+    return n_s;
+}
+
+inline std::string modAlphaCipher::getValidText(const std::string & s)
+{
+    std::string tmp;
+    for (auto c:s) {
+        if (isalpha(c)) {
+            if (islower(c))
+                tmp.push_back(toupper(c));
             else
                 tmp.push_back(c);
         }
     }
     if (tmp.empty())
-        throw cipher_error("Входной текст отстутвует");
+        throw cipher_error("Empty open text");
     return tmp;
-}
-inline std::wstring modAlphaCipher::getValidCipherText(const std::wstring & ws)
-{
-    if (ws.empty())
-        throw cipher_error("Выходной текст отстутвует");
-    for (auto c:ws) {
-        std::string s = codec.to_bytes(ws);
-        if (!iswupper(c))
-            throw cipher_error(std::string("Неправильный тип выходного текста")+s);
-    }
-    return ws;
 }
