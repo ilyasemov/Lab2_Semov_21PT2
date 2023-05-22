e8ce89486171ad47c73b9ba35a4f924eefaca078
ilyasemov <115580012+ilyasemov@users.noreply.github.com>
2023-05-23 00:38:10 +0300

    Update modAlphaCipher.h

--- 2.1/modAlphaCipher.h
+++ 2.1/modAlphaCipher.h
@@ -2,26 +2,23 @@
 #include <vector>
 #include <string>
 #include <map>
-#include <codecvt>
 #include <locale>
+#include <codecvt>
+#include <iostream>
+#include <cctype>
+using namespace std;
 class modAlphaCipher
 {
 private:
-    std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> codec;
-    std::wstring numAlpha = L"ABCDEFGHIJKLMNOPQRSTUVWXYZ";  //алфавит по порядку
-    std::map <wchar_t,int> alphaNum;   //ассоциативный массив "номер по символу"
-    std::vector <int> key;          //ключ
-    std::vector<int> convert(const std::wstring& ws);//преобразование строка-вектор
-    std::wstring convert(const std::vector<int>& v);//преобразование вектор-строка
-    std::wstring getValidKey(const std::wstring & ws);
-    std::wstring getValidOpenText(const std::wstring & ws);
-    std::wstring getValidCipherText(const std::wstring & ws);
+    int key;
+    std::string getValidText(const std::string & s);
 public:
-    modAlphaCipher()=delete;//запретим конструктор без параметров
-    modAlphaCipher(const std::wstring& wskey);  //конструктор для установки ключа
-    std::wstring encrypt(const std::wstring& open_text);  //зашифрование
-    std::wstring decrypt(const std::wstring& cipher_text);//расшифрование
+    modAlphaCipher()=delete;
+    modAlphaCipher(const int& key1) :key(key1) {};
+    std::string encrypt(const std::string& open_text);
+    std::string decrypt(const std::string& cipher_text);
 };
+
 class cipher_error: public std::invalid_argument
 {
 public:
